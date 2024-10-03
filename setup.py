63f9192828cfd7bf95d8e187de3b62b5ac918dbe
John Ritsema <john_ritsema@yahoo.com>
2024-10-03 17:51:47 -0400

    0.1.1 -> adds deps

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bedrock-tools',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
-    install_requires=[],
     author='John Ritsema',
     author_email='john_ritsema@yahoo.com',
     description='A small Python library that simplifies Amazon Bedrock Converse API function calling (i.e., tool use).',
@@ -21,4 +20,8 @@ setup(
     python_requires='>=3.11',
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=[
+        'pydantic==2.9.2',
+        'boto3==1.35.32',
+    ],
 )
